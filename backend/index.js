846d1bde87fbb124a1fc630ee312f79d59e61f3c
menabil <nabil99cc@gmail.com>
2026-07-16 10:29:28 +0600

    add multer

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const {
 } = require("./controllers/todoController");
 const dns = require("node:dns").setServers(["1.1.1.1"], ["8.8.8.8"]);
 const cors = require("cors");
+const multer = require("multer");
 
 mongoose
   .connect(
@@ -17,6 +18,18 @@ mongoose
   .then(() => {
     console.log("DB Connected");
   });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/tmp/my-uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.use(cors());
 
